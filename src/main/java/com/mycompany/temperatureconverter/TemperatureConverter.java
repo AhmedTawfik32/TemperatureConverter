c8eae8a1c3aea1f0643d32117f6032041de7f6c9
Eng. AhmedTawfik <ahmed.tawfik20@gmail.com>
2021-11-05 05:56:29 +0200

    new commit

---
+++ src/main/java/com/mycompany/temperatureconverter/TemperatureConverter.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.temperatureconverter;
+
+/**
+ *
+ * @author AhmedTawfik
+ */
+public class TemperatureConverter {
+
+    public static void main(String[] args) {
+
+        double fahrenheit = 85.0;
+        double celsius;
+
+        celsius = (5.0 / 9) * (fahrenheit - 32);
+
+        System.out.println("The Celsius value is: " + celsius);
+
+    }
+}
